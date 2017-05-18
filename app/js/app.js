e4da17f48dfcb4604fd715b7f7906e3d18fda4f3
Will Elson <elson594@gmail.com>
2017-05-18 22:04:26 +0100

    Added a tasks variable to taskCtrl scope to hold tasks.

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,29 @@ angular
 	.module("todoList", [])
 	.controller("taskCtrl", function($scope) {
 		$scope.message = "Hello, World!";
-	})
+		$scope.tasks = tasks;
+		console.log($scope.tasks.length);
+	});
+
+
+
+var tasks = [
+    {
+        description: "Very bad! Pathetic!", 
+        done: true, 
+        id: 16, 
+        title: "Fake news"
+    }, 
+    {
+        description: "# your code here...", 
+        done: false, 
+        id: 22, 
+        title: "Build a flask web app"
+    }, 
+    {
+        description: "Testy McTestface", 
+        done: false, 
+        id: 24, 
+        title: "Test"
+    }
+];
