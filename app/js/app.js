7be3006a8a557bf36d8aafa4548d749baa50f65b
Will Elson <elson594@gmail.com>
2017-06-07 10:37:39 +0100

    Moved task controller out of app.js and into it's own file.

--- app/js/app.js
+++ app/js/app.js
@@ -1,35 +1,5 @@
 angular
 	.module("todoList", [])
-	.controller("taskCtrl", function($scope) {
-		$scope.tasks = tasks;
-        $scope.activeTask = {};
-        $scope.setActiveTask = setActiveTask;
 
 
-        function setActiveTask(task) {
-            console.log(task.title);
-            $scope.activeTask = task;
-        }
-	});
 
-
-var tasks = [
-    {
-        description: "Very bad! Pathetic!", 
-        done: true, 
-        id: 16, 
-        title: "Fake news"
-    }, 
-    {
-        description: "# your code here...", 
-        done: false, 
-        id: 22, 
-        title: "Build a flask web app"
-    }, 
-    {
-        description: "Testy McTestface", 
-        done: false, 
-        id: 24, 
-        title: "Test"
-    }
-];
