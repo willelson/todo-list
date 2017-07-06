afeeacc732cf6d56f242b1577f0ee5ce7d763c33
Will Elson <elson594@gmail.com>
2017-07-06 14:43:35 +0100

    Added update actice task function. Fixed url variable.

--- app/js/controllers/taskController.js
+++ app/js/controllers/taskController.js
@@ -1,7 +1,7 @@
 angular
 	.module("todoList")
 	.controller("taskCtrl", function($scope, $http) {
-        var url = 'http://127.0.0.1:5000/tasks';
+        var url = server + 'tasks';
 
         $http.get(url).
             then(function(response) {
@@ -11,8 +11,35 @@ angular
         $scope.activeTask = {};
         $scope.setActiveTask = setActiveTask;
 
+        $scope.updateActiveTask = updateActiveTask;
+
         function setActiveTask(task) {
             console.log(task.title);
             $scope.activeTask = task;
         }
+
+        function updateActiveTask(title, description, task_id) {
+            var url = server + 'task/' + task_id
+            var data = {"title": title,
+                        "description": description}
+
+            $http.put(url, data)
+                .success(function (data, status, headers, config) {
+                    for (i = 0; i < $scope.tasks.length; i++) {
+                        if ($scope.tasks[i].id == task.id) {
+                            $scope.tasks[i] = task;
+                        }
+                    }
+            })
+                .error(function (data, status, header, config) {
+                    console.log(status);
+            });
+
+            
+
+        }
+
 	});
+
+
+var server = 'http://127.0.0.1:5000/';
